e35837611af805edd0ca818532560faa91443b7e
Stefan Garcés <stefan.garces@utb.ecutbildning.se>
2020-10-01 23:01:18 +0200

    Lite nytt.

---
+++ Uppgift2/Calc/Comp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift2.Calc
+{
+    public class Comp
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static double Sub(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Multi(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ Uppgift2test/UnitTest1.cs
@@ -0,0 +1,30 @@
+using System;
+using Uppgift2.Calc;
+using Xunit;
+
+namespace Uppgift2test
+{
+    public class UnitTest1
+    {
+        /* Det absolut mest tidkrävande på hela inlämningsuppgiften.
+         * Mycket huvudvärk för att få detta att gå ihop med "public class Comp",
+         * och förstå vad man ska lägga till på "using". Känner att jag måste läsa
+         * på mer och arbeta mer med tester för att få helt kläm på det. Min uppgift
+         * kanske inte var bäst lämpad för test. Jag gjorde en class, "Comp", egentligen
+         * för att få det att fungera med ett test på slutet av projektet.
+         */
+
+        [Fact]
+        public void Add_MathTest()
+        {
+            // Arrange
+            int expect = 25603;
+
+            // act
+            int actual = Comp.Add(2749, 22854);
+
+            // Assert
+            Assert.Equal(expect, actual);
+        }
+    }
+}
